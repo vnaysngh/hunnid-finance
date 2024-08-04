193de30f9051cc2df5fd2a2c5a8c326ccbf9a51d
vnaysngh <singhvinay2525@gmail.com>
2024-08-05 01:17:10 +0530

    in-app signin wallet customised and metadata change

--- src/Wallet.tsx
+++ src/Wallet.tsx
@@ -1,12 +1,21 @@
-import { createWallet } from "thirdweb/wallets";
+import { createWallet, inAppWallet } from "thirdweb/wallets";
 import { client } from "./client";
 import { ConnectButton } from "thirdweb/react";
 import { base } from "thirdweb/chains";
 
+const wallets = [
+  createWallet("io.metamask"),
+  inAppWallet({
+    auth: {
+      options: ["email", "google", "apple", "facebook", "phone"]
+    }
+  })
+];
+
 const Wallet = () => {
   return (
     <ConnectButton
-      wallets={[createWallet("io.metamask")]}
+      wallets={wallets}
       theme="light"
       chain={base}
       client={client}
