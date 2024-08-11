fe4a3d6ac919f9e41a5ca10e966df344c7e5f30e
vnaysngh <singhvinay2525@gmail.com>
2024-08-11 21:16:55 +0530

    handle route refresh bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThirdwebProvider } from "thirdweb/react";
 import { StateContextProvider } from "./context/index.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThirdwebProvider>
       <StateContextProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </StateContextProvider>
     </ThirdwebProvider>
   </React.StrictMode>
