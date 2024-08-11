dc149247db7762b420463a914c442a272a192986
vnaysngh <singhvinay2525@gmail.com>
2024-08-11 19:24:26 +0530

    contracts update

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,6 @@
 export const getContractAddress: any = {
-  "10": "0xbC307E204E6F4D9052cedCcc5Dc69aC7EDaC51E6",
-  "8453": "0x4686609FDc0b3A707422d2e1d6ee26F01f745678"
+  "10": "0xfB9b03b361cDC9A7A40629AAF79fb492f19c3495",
+  "8453": "0x2AE9Efb422d5e7BB7DDF6ea0b94f87114313d1a1"
 };
 
 export const navItems = [
